5900aeb504444cbffb18ceb62c0529ec0a97e6f4
IanaLois <ianaesteron@gmail.com>
2023-04-11 16:00:04 +0000

    Import word list

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
 
 def get_new_word():
     """
     Retrieve new randomized word from imported word list
     """
+    word = random.choice(word_list)
+    return word.upper()
 
 print("""
   _    _                                               
---
+++ words.py
@@ -0,0 +1,8 @@
+word_list = [
+    "apple",
+    "banana",
+    "orange",
+    "grapes",
+    "pineapple",
+    "mango"
+    ]
