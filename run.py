1556345bd1afbb365feda7db51e2def300653396
IanaLois <ianaesteron@gmail.com>
2023-04-11 15:48:01 +0000

    Add core structure of Hangman game

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def get_new_word():
+    """
+    Retrieve new randomized word from imported word list
+    """
+
+print("""
+  _    _                                               
+ | |  | |                                              
+ | |__| |  __ _  _ __    __ _  _ __ ___    __ _  _ __  
+ |  __  | / _` || '_ \  / _` || '_ ` _ \  / _` || '_ \ 
+ | |  | || (_| || | | || (_| || | | | | || (_| || | | |
+ |_|  |_| \__,_||_| |_| \__, ||_| |_| |_| \__,_||_| |_|
+                         __/ |                         
+                        |___/                          
+""")
+
+print("Welcome! Are you ready to play?\n")
+print("Press 'R' to see game rules")
+print("Press 'P' to play game\n")
+
+print("Choose game difficulty\n")
+print("Press 'E' for Easy")
+print("Press 'M' for Moderate")
+print("Press 'C' for Challenging")
+
+def main():
+    """
+    Run all essential functions
+    """
